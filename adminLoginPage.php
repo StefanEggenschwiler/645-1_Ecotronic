6d7a676b820f1ebed390a70cf2dfdece6d9af26d
Sasa Arsic <arsic4brothers@gmail.com>
2015-09-25 15:13:39 +0200

    background added + footer done

--- adminLoginPage.php
+++ adminLoginPage.php
@@ -6,7 +6,7 @@ include_once 'footer.inc';
 <div class="logo"></div>
 <div class="login-block">
     <h1>Login</h1>
-    <input type="text" value="" placeholder="Username" id="username" />
+    <input type="text" value="" placeholder="Username" id="username" /></br></br>
     <input type="password" value="" placeholder="Password" id="password" />
     <button>Submit</button>
 </div>
