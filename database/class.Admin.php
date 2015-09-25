974c2ac0f0f6cc2899812a83df94c6ab6e836a7d
muhamedRedzic <muhamed.r@netplus.ch>
2015-09-25 09:07:27 +0200

    Muhamed

---
+++ database/class.Admin.php
@@ -0,0 +1,52 @@
+<?php
+class Admin
+{
+    private $id;
+    private $firstname;
+    private $lastname;
+    private $username;
+    private $password;
+
+    public function __construct($id, $firstname, $lastname, $username, $password)
+    {
+        $this->id = $id;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    //getter
+    public function getId(){
+        return $this->firstname;
+    }
+    public function getFirstname(){
+        return $this->firstname;
+    }
+    public function getLastname(){
+        return $this->firstname;
+    }
+    public function getUsername(){
+        return $this->firstname;
+    }
+    public function getPassword(){
+        return $this->firstname;
+    }
+
+    //setter
+    public function setId($id){
+        $this->id = $id;
+    }
+    public function setFirstname($firstname){
+        $this->firstname = $firstname;
+    }
+    public function setLastname($lastname){
+        $this->lastname = $lastname;
+    }
+    public function setUsername($username){
+        $this->username = $username;
+    }
+    public function setPassword($password){
+        $this->password = $password;
+    }
+}
