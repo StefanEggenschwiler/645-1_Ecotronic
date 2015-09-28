cf2c02e4b492e8aa5c4032a9d30d5930ee07ecfe
pokito10 <jeremie.vianin@students.hevs.ch>
2015-09-28 10:41:25 +0200

    Jeremie - Vertical Menu Bar

--- index.php
+++ index.php
@@ -12,46 +12,64 @@ $classifications = array("A+++", "A++","A+", "A", "B", "C", "D");
 $consumptions = array();
 
 ?>
-<ul>
-    <li><?php $translate->__('Category')?>
+
+<div class="wrapper">
+    <nav class="vertical">
         <ul>
-            <?php foreach($categories as $value){
-                echo "<li>";
-                echo $translate->__($value);
-                echo "</li>";
-            } ?>
+            <li>
+                <a href="#"><?php $translate->__('Category')?></a>
+                <div>
+                    <ul>
+                        <?php foreach($categories as $value){
+                            echo "<li><a href='#'>";
+                            echo $translate->__($value);
+                            echo "</a></li>";
+                        } ?>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <a href="#"><?php $translate->__('Brand')?></a>
+                <div>
+                    <ul>
+                        <?php foreach($brands as $value){
+                            echo "<li><a href='#'>";
+                            echo $translate->__($value);
+                            echo "</a></li>";
+                        } ?>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <a href="#"><?php $translate->__('Classification')?></a>
+                <div>
+                    <ul>
+                        <?php foreach($classifications as $value){
+                            echo "<li><a href='#'>";
+                            echo $translate->__($value);
+                            echo "</a></li>";
+                        } ?>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <a href="#"><?php $translate->__('kWh/year')?></a>
+                <div>
+                    <ul>
+
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <a href="#"><?php $translate->__('Price')?></a>
+                <div>
+                    <ul>
+
+                    </ul>
+                </div>
+            </li>
         </ul>
-    </li>
-    <li><?php $translate->__('Brand')?>
-        <ul>
-            <?php foreach($brands as $value){
-                echo "<li>";
-                echo $translate->__($value);
-                echo "</li>";
-            } ?>
-        </ul>
-    </li>
-    <li><?php $translate->__('Classification')?>
-        <ul>
-            <?php foreach($classifications as $value){
-                echo "<li>";
-                echo $translate->__($value);
-                echo "</li>";
-            } ?>
-        </ul>
-    </li>
-    <li><?php $translate->__('kWh/year')?>
-        <ul>
-            <?php foreach($classifications as $value){
-                echo "<li>";
-                echo $translate->__($value);
-                echo "</li>";
-            } ?>
-        </ul>
-    </li>
-    <li><?php $translate->__('Price')?>
-    </li>
-</ul>
+    </nav>
 <?php
 include_once 'footer.inc';
 ?>
