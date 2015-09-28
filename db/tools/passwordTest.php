561720d05478bcf42681286c524800d1077f79c8
Stefan Eggenschwiler <stefan.eggenschwiler@students.hevs.ch>
2015-09-28 10:08:54 +0200

    bin.

--- db/tools/passwordTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hash = password_hash("12345", PASSWORD_BCRYPT);
-echo $hash."<br>";
-
-if (password_verify('12345', $hash)) {
-    echo 'Password is valid!';
-} else {
-    echo 'Invalid password.';
-}
