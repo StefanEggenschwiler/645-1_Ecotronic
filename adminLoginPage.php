2d68a320cff96c80f16a5f6acb15ae637405073d
Sasa Arsic <arsic4brothers@gmail.com>
2015-09-28 11:26:47 +0200

    test

--- adminLoginPage.php
+++ adminLoginPage.php
@@ -5,7 +5,7 @@ include_once 'footer.inc';
 
 <div class="logo"></div>
 
-<div class="loginBlock fullCenter">
+<div class="loginBlock centered">
     <h1>Admin login</h1>
     <input type="text" value="" placeholder="Username" id="username" /> </br></br>
     <input type="password" value="" placeholder="Password" id="password" />
