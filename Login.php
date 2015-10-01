5184d82a6d4aff181267fcad1652efc10714852f
Stefan Eggenschwiler <stefan.eggenschwiler@students.hevs.ch>
2015-10-01 09:04:22 +0200

    Rollback Login

--- Login.php
+++ Login.php
@@ -5,13 +5,12 @@ include_once 'footer.inc';
 
 
 <div class="loginBlock centered">
-    <h1>Admin login</h1>
-    <form action="login.php" method="post">
-        <input type="text" value="" placeholder="Username" id="username" /> </br></br>
-        <input type="password" value="" placeholder="Password" id="password" />
-
-    </form>
-    </br>
-    </br>
-    <button>Give me access</button>
+    <h1>Admin Login</h1>
+        <form method="post" action="AdminPanel.php">
+            <input type="text" name="user" placeholder="Username" id="username" /> </br></br>
+            <input type="password" name="pwd" placeholder="Password" id="password" />
+            </br>
+            </br>
+            <button type="submit" value="login" name="action">Login</button>
+        </form>
 </div>
