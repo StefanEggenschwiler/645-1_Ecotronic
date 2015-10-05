70a7efbb47b7d1a729bd46956018264b6453a31d
Stefan Eggenschwiler <stefan.eggenschwiler@students.hevs.ch>
2015-10-05 14:33:10 +0200

    Added translation page. (Still need to escape special characters)

--- redirect.php
+++ redirect.php
@@ -5,6 +5,8 @@ require_once 'database/class.Model.php';
 if (isset ( $_POST ['action'] )) {
     if ($_POST ['action'] == 'login') {
         authenticate(); // the user is logging in
+    } else if ($_POST ['action'] == 'translationSubmit') {
+        saveTranslationTable();
     }
 } else {
     echo 'ACCESS DENIED!';
@@ -29,7 +31,28 @@ function authenticate() {
     exit;
 }
 
+function saveTranslationTable() {
+    $german = array();
+    $french = array();
+    $en = "";
+    foreach($_POST as $inputName => $inputValue)
+    {
+        if(strpos("x" . $inputName,'cell') !== false) {
+            if (strpos("x" . $inputName, '_en_') !== false) {
+                $en = $inputValue;
+            } else if (strpos("x" . $inputName, '_de_') !== false) {
+                $german[] = $en . "=" . $inputValue . "\r\n";
+            } else if (strpos("x" . $inputName, '_fr_') !== false) {
+                $french[] = $en . "=" . $inputValue . "\r\n";
+            }
+        }
+    }
+    file_put_contents('translations/de_2.txt', $german);
+    file_put_contents('translations/fr_2.txt', $french);
 
+    header ('location: editTranslation.php');
+    exit;
+}
 ?>
 
 
