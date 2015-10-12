24928eb091022490900d09bfb4c73e9254020352
Sasa Arsic <arsic4brothers@gmail.com>
2015-10-12 11:12:34 +0200

    started working on the page "Update/Delete"

---
+++ updateDeleteDevice.php
@@ -0,0 +1,35 @@
+<?php
+include_once 'headerAdmin.inc';
+require_once 'database/class.Model.php';
+require_once 'dto/class.Type.php';
+require_once 'dto/class.Brand.php';
+require_once 'dto/class.Device.php';
+require_once 'dto/class.EfficiencyClass.php';
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    deleteDevice();
+    updateDevice();
+}
+
+
+function deleteDevice() {
+
+}
+
+function updateDevice() {
+
+}
+?>
+
+
+<div>
+    <div id="container">
+
+    </div>
+
+    </body>
+
+
+
+</div>
