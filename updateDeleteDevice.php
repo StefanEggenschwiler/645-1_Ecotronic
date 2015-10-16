d774942d7cf8f494f09d997c1e6ca244ac568274
Sasa Arsic <arsic4brothers@gmail.com>
2015-10-16 13:49:58 +0200

    started working on the page "Update/Delete"

--- updateDeleteDevice.php
+++ updateDeleteDevice.php
@@ -1,34 +1,119 @@
 <?php
 include_once 'headerAdmin.inc';
-require_once 'database/class.Model.php';
-require_once 'dto/class.Type.php';
-require_once 'dto/class.Brand.php';
-require_once 'dto/class.Device.php';
-require_once 'dto/class.EfficiencyClass.php';
 
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    deleteDevice();
-    updateDevice();
-}
-
-
-function deleteDevice() {
+function deleteDevice()
+{
 
 }
 
-function updateDevice() {
+function updateDevice()
+{
 
 }
+
 ?>
 
+<div class="shell980W">
+    </br>
+        <div class="box">
 
-<div>
-    <div id="container">
+            <div class="box-head">
+                <h2 class="left">All Articles</h2>
 
+                <div class="right">
+                    <label>search articles</label>
+                    <input type="text" class="field small-field"/>
+                    <input type="submit" class="button" value="search"/>
+                </div>
+            </div>
+
+            <div class="table">
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                        <th width="13"><input type="checkbox" class="checkbox"/></th>
+                        <th>Title</th>
+                        <th>Date</th>
+                        <th>Added by</th>
+                        <th width="110" class="ac">Content Control</th>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" class="checkbox"/></td>
+                        <td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+                        <td>12.05.09</td>
+                        <td><a href="#">Administrator</a></td>
+                        <td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+                    </tr>
+                    <tr class="odd">
+                        <td><input type="checkbox" class="checkbox"/></td>
+                        <td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+                        <td>12.05.09</td>
+                        <td><a href="#">Administrator</a></td>
+                        <td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" class="checkbox"/></td>
+                        <td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+                        <td>12.05.09</td>
+                        <td><a href="#">Administrator</a></td>
+                        <td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+                    </tr>
+                    <tr class="odd">
+                        <td><input type="checkbox" class="checkbox"/></td>
+                        <td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+                        <td>12.05.09</td>
+                        <td><a href="#">Administrator</a></td>
+                        <td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" class="checkbox"/></td>
+                        <td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+                        <td>12.05.09</td>
+                        <td><a href="#">Administrator</a></td>
+                        <td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+                    </tr>
+                    <tr class="odd">
+                        <td><input type="checkbox" class="checkbox"/></td>
+                        <td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+                        <td>12.05.09</td>
+                        <td><a href="#">Administrator</a></td>
+                        <td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" class="checkbox"/></td>
+                        <td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+                        <td>12.05.09</td>
+                        <td><a href="#">Administrator</a></td>
+                        <td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+                    </tr>
+                    <tr class="odd">
+                        <td><input type="checkbox" class="checkbox"/></td>
+                        <td><h3><a href="#">Lorem ipsum dolor sit amet, consectetur.</a></h3></td>
+                        <td>12.05.09</td>
+                        <td><a href="#">Administrator</a></td>
+                        <td><a href="#" class="ico del">Delete</a><a href="#" class="ico edit">Edit</a></td>
+                    </tr>
+                </table>
+
+
+                <div class="pagging">
+                    <div class="left">Showing 1-12 of 44</div>
+                    <div class="right">
+                        <a href="#">Previous</a>
+                        <a href="#">1</a>
+                        <a href="#">2</a>
+                        <a href="#">3</a>
+                        <a href="#">4</a>
+                        <a href="#">245</a>
+                        <span>...</span>
+                        <a href="#">Next</a>
+                        <a href="#">View all</a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
     </div>
-</div>
 
-<?php
-include_once 'footer.inc';
-?>
+
