45580ec753fb3de7443502aa282a81efe2ccd763
muhamedRedzic <muhamed.r@netplus.ch>
2015-10-19 10:55:02 +0200

    comparison table with CSS

--- comparison.php
+++ comparison.php
@@ -2,17 +2,25 @@
 include_once 'headerIndex.inc';
 require_once 'dto/class.Device.php';
 ?>
-<table>
+<table class="comparisonTable">
     <tr>
-        <th><?php $translate->__('Your old device')?>-></th>
+        <td><?php $translate->__('Your old device')?>-></td>
         <?php
         foreach ($_SESSION['comparedDevices'] as $value){
-            echo '<th><input type="radio" name="dev" value="';
+            echo '<td><input type="radio" name="dev" value="';
             echo $value->getSerialNumber().'"';
                 if($value->getSerialNumber() == $_SESSION['myOldDevice']){
                     echo ' checked';
                 }
-            echo '></th>';
+            echo '></td>';
+        }
+        ?>
+    </tr>
+    <tr>
+        <td></td>
+        <?php
+        foreach ($_SESSION['comparedDevices'] as $value){
+            echo '<td><img src="'.$value->getImage().'"</td>';
         }
         ?>
     </tr>
@@ -45,6 +53,62 @@ require_once 'dto/class.Device.php';
         <?php
         foreach ($_SESSION['comparedDevices'] as $value){
             echo '<td>'.$value->getPrice().'</td>';
+        }
+        ?>
+    </tr>
+    <tr>
+        <td><?php $translate->__('Energy price')?></td>
+        <?php
+        foreach ($_SESSION['comparedDevices'] as $value){
+            echo '<td>'.$value->getEnergyPrice().'</td>';
+        }
+        ?>
+    </tr>
+    <tr>
+        <td><?php $translate->__('Energy consumption')?></td>
+        <?php
+        foreach ($_SESSION['comparedDevices'] as $value){
+            echo '<td>'.$value->getEnergyConsumption().'</td>';
+        }
+        ?>
+    </tr>
+    <tr>
+        <td><?php $translate->__('Production year')?></td>
+        <?php
+        foreach ($_SESSION['comparedDevices'] as $value){
+            echo '<td>'.$value->getProductionYear().'</td>';
+        }
+        ?>
+    </tr>
+    <tr>
+        <td><?php $translate->__('Serial number')?></td>
+        <?php
+        foreach ($_SESSION['comparedDevices'] as $value){
+            echo '<td>'.$value->getSerialNumber().'</td>';
+        }
+        ?>
+    </tr>
+    <tr>
+        <td><?php $translate->__('Manufacturer link')?></td>
+        <?php
+        foreach ($_SESSION['comparedDevices'] as $value){
+            echo '<td><a href="';
+            echo $value->getManufacturerLink();
+            echo '" >';
+            echo $translate->__('Link');
+            echo '</a></td>';
+        }
+        ?>
+    </tr>
+    <tr>
+        <td><?php $translate->__('Shop link')?></td>
+        <?php
+        foreach ($_SESSION['comparedDevices'] as $value){
+            echo '<td><a href="';
+            echo $value->getShopLink();
+            echo '" >';
+            echo $translate->__('Link');
+            echo '</a></td>';
         }
         ?>
     </tr>
