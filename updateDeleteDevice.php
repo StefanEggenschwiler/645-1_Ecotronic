92276f96fbf9814e58c8180b32d3810ac73df2ee
Sasa Arsic <arsic4brothers@gmail.com>
2015-10-20 00:06:22 +0200

    Update delete page has now all devices displayed

--- updateDeleteDevice.php
+++ updateDeleteDevice.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'headerAdmin.inc';
 require_once 'database/class.Model.php';
-
+require_once 'dto/class.Device.php';
 
 $model = new Model();
 $types = $model->getAllTypes();
@@ -9,6 +9,7 @@ $brands = $model->getAllBrands();
 $efficiencyClasses = $model->getAllEfficiencyClasses();
 $consumptions = array();
 $showedItems = $model->getAllBrands();
+$allItemsObjects = $model->getAllDevices();
 
 function deleteDevice()
 {
@@ -38,8 +39,8 @@ function updateDevice()
 
                 <table width="100%" border="0" cellspacing="0" cellpadding="0">
                     <tr>
-                        <th width="13"><input type="checkbox" class="checkbox"/></th>
-                        <th id="selectAll">Select all</th>
+                        <th width="13"><input type="checkbox" class="checkbox"></th>
+                        <th id="selectAll"/>Select all</th>
 
                         <h1>FILTERS</h1>
                         </br>
@@ -57,18 +58,28 @@ function updateDevice()
                                 echo '<option>'. $value->getBrandName().'</option>';
                             } ?>
                         </select>
+
+
+                        <select name="selectEfficiencyClassName" style="width: 200px">
+                            <?php
+                            foreach ($efficiencyClasses as $value) {
+                                echo '<option>' . $value->getClassName() . '</option>';
+                            } ?>
+                        </select>
                     </tr>
 
                     <tr>
-                        <th width="13"><input type="checkbox" class="checkbox"/></th>
+                        <th></th>
                         <th>Type</th>
                         <th>Brand</th>
                         <th>Model</th>
                         <th>Serial number</th>
                         <th>Production Year</th>
+                        <th>Life Span (in years)</th>
                         <th>Efficiency class</th>
+                        <th>Energy price (kWh/year)</th>
                         <th>Energy consumption</th>
-                        <th>Price</th>
+                        <th>Price (in CHF)</th>
                         <th>Image URL</th>
                         <th>Manufacturer link</th>
                         <th>Shop link</th>
@@ -76,20 +87,34 @@ function updateDevice()
                     </tr>
 
 
+
                     <tr>
-                        <td><input type="checkbox" class="checkbox"/></td>
-                        <td><h3>....</h3></td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td>....</td>
-                        <td><a href="#" class="ico del">Delete</a>  <a href="#" class="ico edit">Edit</a></td>
+
+                            <?php
+                            foreach ($allItemsObjects as $items) {
+                                echo'<td><input type="checkbox" class="checkbox"/>';
+
+                                echo '<td>'. $items->getTypeName() . '</td>';
+                                echo '<td>'. $items->getBrandName() . '</td>';
+                                echo '<td>'. $items->getModel() . '</td>';
+                                echo '<td>'. $items->getSerialNumber() . '</td>';
+                                echo '<td>'. $items->getProductionYear() . '</td>';
+                                echo '<td>'. $items->getLifeSpan() . '</td>';
+                                echo '<td>'. $items->getEfficiencyClassName() . '</td>';
+                                echo '<td>'. $items->getEnergyPrice() . '</td>';
+                                echo '<td>'. $items->getEnergyConsumption() . '</td>';
+                                echo '<td>'. $items->getPrice() . '</td>';
+                                echo '<td>'. $items->getImage() . '</td>';
+                                echo '<td>'. $items->getManufacturerLink() . '</td>';
+                                echo '<td>'. $items->getShopLink() . '</td>';
+                                echo '<td><a href="#" class="ico del">Delete</a>  <a href="#" class="ico edit">Edit</a> </td>';
+
+                                echo'<tr>';
+
+                            }
+
+                            ?>
+
                     </tr>
 
                 </table>
